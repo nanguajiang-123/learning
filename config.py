da09fcd7c4749aeef7636b26b21ba287a28d6aa5
nanguajiang-123 <u202440095@xs.ustb.edu.cn>
2025-12-16 18:26:38 +0800

    feat:启动bug

--- config.py
+++ config.py
@@ -5,8 +5,15 @@ import os
 load_dotenv()
 
 DATABASE_CONFIG = {
-    "db_url": f"postgres://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}",
-    "modules": {"models": ["models"]},  # 模型模块路径
+    "connections": {
+        "default": f"postgres://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}",
+    },
+    "apps": {
+        "models": {
+            "models": ["models"],
+            "default_connection": "default",
+        },
+    },
     "generate_schemas": True,               # 自动生成数据库表（开发环境用，生产环境建议关闭）
     "add_exception_handlers": True,         # 添加异常处理器
 }
